a47b34c5ec5b5e6fb7f4f094cf0af78552eca279
Said <ahmed0mobarez@gmail.com>
2019-10-03 09:35:32 +0200

    Task 1 first commit

---
+++ Task1/Task1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Task1.c
+ *
+ * Created: 10/3/2019 9:34:04 AM
+ * Author : Ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
